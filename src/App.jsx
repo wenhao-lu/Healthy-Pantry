d8134e35c97371ce9b6e405e390aba8446d990e4
wenhaolu <wenhaolu.kevin@gmail.com>
2024-07-21 13:06:51 -0400

    style static pages

--- src/App.jsx
+++ src/App.jsx
@@ -20,31 +20,33 @@ import SidebarLayout from './ui/SidebarLayout';
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
+      <div className="flex min-h-screen flex-col items-center gap-6 px-4 py-6">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="app" element={<AppLayout />}>
-          <Route element={<BasicLayout />}>
-            <Route path="stocks" element={<Stocks />} />
-            <Route path="recipes" element={<Recipes />} />
-            <Route path="recipes/:id" element={<RecipeDetail />} />
-          </Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route element={<BasicLayout />}>
+              <Route path="stocks" element={<Stocks />} />
+              <Route path="recipes" element={<Recipes />} />
+              <Route path="recipes/:id" element={<RecipeDetail />} />
+            </Route>
 
-          <Route element={<SidebarLayout />}>
-            <Route path="dashboard" element={<Dashboard />}>
-              <Route index element={<Status />} />
-              <Route path="stocklist" element={<StockList />} />
-              <Route path="recipelist" element={<RecipeList />} />
-              <Route path="account" element={<Account />} />
+            <Route element={<SidebarLayout />}>
+              <Route path="dashboard" element={<Dashboard />}>
+                <Route index element={<Status />} />
+                <Route path="stocklist" element={<StockList />} />
+                <Route path="recipelist" element={<RecipeList />} />
+                <Route path="account" element={<Account />} />
+              </Route>
             </Route>
           </Route>
-        </Route>
 
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }

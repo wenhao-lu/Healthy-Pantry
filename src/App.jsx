904ff4b251be7b20692c398fb7ae464a29c8a0d6
wenhaolu <wenhaolu.kevin@gmail.com>
2024-07-29 21:15:59 -0400

    react-query dev-tool

--- src/App.jsx
+++ src/App.jsx
@@ -15,30 +15,41 @@ import RecipeList from './components/RecipeList';
 import Account from './pages/Account';
 import PageNotFound from './ui/PageNotFound';
 import DashboardLayout from './ui/DashboardLayout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <BrowserRouter>
-      <RootLayout>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/login" element={<Login />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <RootLayout>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
 
-          <Route path="/stocks" element={<Stocks />} />
-          <Route path="/recipes" element={<Recipes />} />
-          <Route path="/recipes/:id" element={<RecipeDetail />} />
+            <Route path="/stocks" element={<Stocks />} />
+            <Route path="/recipes" element={<Recipes />} />
+            <Route path="/recipes/:id" element={<RecipeDetail />} />
 
-          <Route path="/dashboard" element={<DashboardLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="stocklist" element={<StockList />} />
-            <Route path="recipelist" element={<RecipeList />} />
-            <Route path="account" element={<Account />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </RootLayout>
-    </BrowserRouter>
+            <Route path="/dashboard" element={<DashboardLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="stocklist" element={<StockList />} />
+              <Route path="recipelist" element={<RecipeList />} />
+              <Route path="account" element={<Account />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </RootLayout>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 

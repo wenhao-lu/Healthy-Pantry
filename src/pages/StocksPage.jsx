490466e74d94ffd3f2bb58e76acec2f670ea2b60
wenhaolu <wenhaolu.kevin@gmail.com>
2024-08-02 16:29:03 -0400

    search recipe and get random recipe on stocks page

--- src/pages/StocksPage.jsx
+++ src/pages/StocksPage.jsx
@@ -10,17 +10,17 @@ function StocksPage() {
   const [stockQuantity, setStockQuantity] = useState(0);
   const [stockUnit, setStockUnit] = useState('g');
   const [recipes, setRecipes] = useState([]);
-  const [randomRecipes, setRandomRecipes] = [];
+  const [randomRecipes, setRandomRecipes] = useState([]);
 
   useEffect(function () {
     async function fetchRandomRecipes() {
       try {
         const res = await fetch(
-          `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}&health=low-fat-abs&random=true`,
+          `https://api.edamam.com/api/recipes/v2?type=public&app_id=${APP_ID}&app_key=${APP_KEY}&diet=low-fat&random=true`,
         );
         const data = await res.json();
         console.log(data);
-        //setsetRandomRecipes(data);
+        setRandomRecipes(data.hits);
       } catch (err) {
         console.error(err.message);
       }
@@ -179,47 +179,84 @@ function StocksPage() {
         <Spinner />
       ) : error ? (
         <div>Error: {error.message}</div>
-      ) : (
-        recipes.length > 0 && (
-          <div className="bg-green-50 px-4 py-4">
-            <h2 className="ml-auto mr-auto w-96 pb-2 text-xl font-semibold">
-              Recipes😋:
-            </h2>
-            <div className="ml-auto mr-auto flex w-96 flex-row flex-wrap items-center justify-center gap-4">
-              {recipes.map((recipe, index) => (
-                <div
-                  key={index}
-                  className="w-44 border-b-[0.1rem] border-gray-200"
-                >
-                  <a href={recipe.recipe.url} target="_blank">
-                    <img
-                      src={recipe.recipe.image}
-                      alt={recipe.recipe.label}
-                      className="rounded-md border border-gray-200 shadow-sm transition-all hover:opacity-60"
-                    />
-                    <p className="text-[0.8rem] font-semibold">
-                      {recipe.recipe.label}
-                    </p>
-                  </a>
-                  <div className="flex flex-row items-center justify-between gap-2 pb-2 text-xs italic">
-                    <p className="capitalize">
-                      <span className="text-green-600">
-                        {Math.round(Number(recipe.recipe.calories))}
-                      </span>{' '}
-                      calories
-                    </p>
-                    <p className="capitalize">
-                      <span className="text-green-600">
-                        {recipe.recipe.ingredients.length}
-                      </span>{' '}
-                      ingredients
-                    </p>
-                  </div>
+      ) : recipes.length > 0 ? (
+        <div className="bg-green-50 px-4 py-4">
+          <h2 className="ml-auto mr-auto w-96 pb-2 text-xl font-semibold">
+            Recipes😋:
+          </h2>
+          <div className="ml-auto mr-auto flex w-96 flex-row flex-wrap items-center justify-center gap-4">
+            {recipes.map((recipe, index) => (
+              <div
+                key={index}
+                className="w-44 border-b-[0.1rem] border-gray-200"
+              >
+                <a href={recipe.recipe.url} target="_blank">
+                  <img
+                    src={recipe.recipe.image}
+                    alt={recipe.recipe.label}
+                    className="rounded-md border border-gray-200 shadow-sm transition-all hover:opacity-60"
+                  />
+                  <p className="h-10 text-[0.8rem] font-semibold">
+                    {recipe.recipe.label}
+                  </p>
+                </a>
+                <div className="flex flex-row items-center justify-between gap-2 pb-2 text-xs italic">
+                  <p className="capitalize">
+                    <span className="text-green-600">
+                      {Math.round(Number(recipe.recipe.calories))}
+                    </span>{' '}
+                    calories
+                  </p>
+                  <p className="capitalize">
+                    <span className="text-green-600">
+                      {recipe.recipe.ingredients.length}
+                    </span>{' '}
+                    ingredients
+                  </p>
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-        )
+        </div>
+      ) : (
+        <div className="bg-green-50 px-4 py-4">
+          <h2 className="ml-auto mr-auto w-96 pb-2 text-xl font-semibold">
+            Recipes😋:
+          </h2>
+          <div className="ml-auto mr-auto flex w-96 flex-row flex-wrap items-center justify-center gap-4">
+            {randomRecipes.map((randomRecipe, index) => (
+              <div
+                key={index}
+                className="w-44 border-b-[0.1rem] border-gray-200"
+              >
+                <a href={randomRecipe.recipe.url} target="_blank">
+                  <img
+                    src={randomRecipe.recipe.image}
+                    alt={randomRecipe.recipe.label}
+                    className="rounded-md border border-gray-200 shadow-sm transition-all hover:opacity-60"
+                  />
+                  <p className="h-10 text-[0.8rem] font-semibold">
+                    {randomRecipe.recipe.label}
+                  </p>
+                </a>
+                <div className="flex flex-row items-center justify-between gap-2 pb-2 text-xs italic">
+                  <p className="capitalize">
+                    <span className="text-green-600">
+                      {Math.round(Number(randomRecipe.recipe.calories))}
+                    </span>{' '}
+                    calories
+                  </p>
+                  <p className="capitalize">
+                    <span className="text-green-600">
+                      {randomRecipe.recipe.ingredients.length}
+                    </span>{' '}
+                    ingredients
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       )}
     </div>
   );

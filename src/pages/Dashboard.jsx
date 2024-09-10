8d0cf89ac18a326e710c5c0860fbf41f688144ff
wenhaolu <wenhaolu.kevin@gmail.com>
2024-09-10 19:51:42 -0400

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,8 @@ import { HiOutlineBriefcase } from 'react-icons/hi2';
 import { getStocks } from '../services/apiStocks';
 import Spinner from '../ui/Spinner';
 import { getRecipes } from '../services/apiRecipes';
+import { GiFruitBowl } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
 
 function Dashboard() {
   const queryClient = useQueryClient();
@@ -29,16 +31,18 @@ function Dashboard() {
     <>
       <div className="text-l pb-5 font-semibold leading-5">Dashboard</div>
       <div className="flex flex-col justify-center text-xs">
-        <div className="grid grid-cols-4 gap-3">
+        <div className="grid grid-cols-2 gap-3">
           <div className="rounded-md border-gray-100 bg-gray-50 p-4">
             <div className="relative grid grid-cols-3 items-center gap-3">
-              <div className="ml-[-0.5rem] flex h-7 w-7 items-center justify-center rounded-full bg-blue-100">
-                <HiOutlineBriefcase />
+              <div className="ml-[-0.5rem] flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
+                <GiFruitBowl />
               </div>
               <div>
-                <h5 className="mb-[-0.4rem] self-end text-[0.5rem] font-semibold uppercase tracking-wide text-gray-500">
-                  Stocks
-                </h5>
+                <Link to="/dashboard/stocklist">
+                  <h5 className="self-end text-[0.5rem] font-semibold uppercase tracking-wide text-gray-500 hover:text-gray-400">
+                    Stocks
+                  </h5>
+                </Link>
                 <div className="text-sm">
                   {loadingStocks ? (
                     <Spinner />
@@ -54,13 +58,15 @@ function Dashboard() {
 
           <div className="rounded-md border-gray-100 bg-gray-50 p-4">
             <div className="relative grid grid-cols-3 items-center gap-3">
-              <div className="ml-[-0.5rem] flex h-7 w-7 items-center justify-center rounded-full bg-blue-100">
+              <div className="ml-[-0.5rem] flex h-10 w-10 items-center justify-center rounded-full bg-blue-100">
                 <HiOutlineBriefcase />
               </div>
               <div>
-                <h5 className="mb-[-0.4rem] self-end text-[0.5rem] font-semibold uppercase tracking-wide text-gray-500">
-                  Recipes
-                </h5>
+                <Link to="/dashboard/recipelist">
+                  <h5 className="self-end text-[0.5rem] font-semibold uppercase tracking-wide text-gray-500 hover:text-gray-400">
+                    Recipes
+                  </h5>
+                </Link>
                 <div className="text-sm">
                   {loadingRecipes ? (
                     <Spinner />
@@ -70,34 +76,6 @@ function Dashboard() {
                     recipes.length
                   )}
                 </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-md border-gray-100 bg-gray-50 p-4">
-            <div className="relative grid grid-cols-3 items-center gap-3">
-              <div className="ml-[-0.5rem] flex h-7 w-7 items-center justify-center rounded-full bg-blue-100">
-                <HiOutlineBriefcase />
-              </div>
-              <div>
-                <h5 className="mb-[-0.4rem] self-end text-[0.5rem] font-semibold uppercase tracking-wide text-gray-500">
-                  title
-                </h5>
-                <p className="text-sm">stat</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="rounded-md border-gray-100 bg-gray-50 p-4">
-            <div className="relative grid grid-cols-3 items-center gap-3">
-              <div className="ml-[-0.5rem] flex h-7 w-7 items-center justify-center rounded-full bg-blue-100">
-                <HiOutlineBriefcase />
-              </div>
-              <div>
-                <h5 className="mb-[-0.4rem] self-end text-[0.5rem] font-semibold uppercase tracking-wide text-gray-500">
-                  title
-                </h5>
-                <p className="text-sm">stat</p>
               </div>
             </div>
           </div>
